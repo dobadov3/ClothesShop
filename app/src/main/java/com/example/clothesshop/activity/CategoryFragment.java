88cff0b303fe21be9f7d5335dbb123efa68c178c
dobadov3 <70206718+dobadov3@users.noreply.github.com>
2020-12-08 20:18:17 +0700

    Add Category Fragment

--- app/src/main/java/com/example/clothesshop/activity/CategoryFragment.java
+++ app/src/main/java/com/example/clothesshop/activity/CategoryFragment.java
@@ -3,12 +3,22 @@ package com.example.clothesshop.activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.clothesshop.R;
+import com.example.clothesshop.adapter.HomeAdapter;
+import com.example.clothesshop.model.Clothes;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -57,10 +67,122 @@ public class CategoryFragment extends Fragment {
         }
     }
 
+    RecyclerView recyclerViewProduct;
+    ArrayList<Clothes> mclothes = new ArrayList<>();
+    HomeAdapter adapter;
+    ImageButton imageButtonTops;
+    ImageButton imageButtonBottoms;
+    ImageButton imageButtonBags;
+    ImageButton imageButtonHats;
+    ImageButton imageButtonSales;
+    TextView tvTitle;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false);
+        View view = inflater.inflate(R.layout.fragment_category, container, false);
+        recyclerViewProduct = view.findViewById(R.id.recyclerViewProducts);
+
+        imageButtonBottoms = view.findViewById(R.id.imgBtnBottoms);
+        imageButtonTops = view.findViewById(R.id.imgBtnTop);
+        imageButtonBags = view.findViewById(R.id.imgBtnBags);
+        imageButtonHats = view.findViewById(R.id.imgBtnHats);
+        imageButtonSales = view.findViewById(R.id.imgBtnSales);
+        tvTitle = view.findViewById(R.id.tvTitle);
+
+        imageButtonTops.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickTops(v);
+            }
+        });
+
+        imageButtonBottoms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickBottoms(v);
+            }
+        });
+
+        imageButtonBags.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickBags(v);
+            }
+        });
+
+        imageButtonHats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickHats(v);
+            }
+        });
+
+        imageButtonSales.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickSales(v);
+            }
+        });
+
+        onClickTops(view);
+        return view;
+    }
+    private void AddClothes(){
+        mclothes.add(new Clothes("Áo 1", 400000, R.drawable.image_2));
+        mclothes.add(new Clothes("Áo 2", 300000, R.drawable.image_3));
+        mclothes.add(new Clothes("Áo 3", 250000, R.drawable.image_4));
+        mclothes.add(new Clothes("Áo 4", 500000, R.drawable.image_5));
+        mclothes.add(new Clothes("Áo 1", 400000, R.drawable.image_2));
+        mclothes.add(new Clothes("Áo 2", 300000, R.drawable.image_3));
+        mclothes.add(new Clothes("Áo 3", 250000, R.drawable.image_4));
+        mclothes.add(new Clothes("Áo 4", 500000, R.drawable.image_5));
+        mclothes.add(new Clothes("Áo 1", 400000, R.drawable.image_2));
+    }
+    private void onClickTops(View view)
+    {
+        mclothes.clear();
+        tvTitle.setText("TOPS");
+        AddClothes();
+        adapter = new HomeAdapter(mclothes);
+        recyclerViewProduct.setLayoutManager(new GridLayoutManager(getContext(), 3));
+        recyclerViewProduct.setAdapter(adapter);
+    }
+    private void onClickBottoms(View view)
+    {
+        mclothes.clear();
+        tvTitle.setText("BOTTOMS");
+        AddClothes();
+        adapter = new HomeAdapter(mclothes);
+        recyclerViewProduct.setLayoutManager(new GridLayoutManager(getContext(), 3));
+        recyclerViewProduct.setAdapter(adapter);
+    }
+    private void onClickBags(View view)
+    {
+        mclothes.clear();
+        tvTitle.setText("BAGS");
+        AddClothes();
+        adapter = new HomeAdapter(mclothes);
+        recyclerViewProduct.setLayoutManager(new GridLayoutManager(getContext(), 3));
+        recyclerViewProduct.setAdapter(adapter);
+    }
+    private void onClickHats(View view)
+    {
+        mclothes.clear();
+        tvTitle.setText("HATS");
+        AddClothes();
+        adapter = new HomeAdapter(mclothes);
+        recyclerViewProduct.setLayoutManager(new GridLayoutManager(getContext(), 3));
+        recyclerViewProduct.setAdapter(adapter);
+    }
+    private void onClickSales(View view)
+    {
+        mclothes.clear();
+        tvTitle.setText("SALES");
+        AddClothes();
+        adapter = new HomeAdapter(mclothes);
+        recyclerViewProduct.setLayoutManager(new GridLayoutManager(getContext(), 3));
+        recyclerViewProduct.setAdapter(adapter);
     }
 }
